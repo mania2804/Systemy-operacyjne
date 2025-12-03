4ed655c24abbf10fce514bb7f5c2de01c6c617ec
mania2804 <dziedzicmaria2@gmail.com>
2025-12-03 11:11:45 +0100

    Update moje 4.c

--- moje 4.c
+++ moje 4.c
@@ -4,118 +4,97 @@
 #include <sys/time.h>
 
 typedef long long ll;
-ll suma_watków=0;
 
 typedef struct {
-    int *tab;
+    int *arr;
     long start;
     long end;
-    ll result;
-}Watek;
+    ll *result;
+} ThreadData;
 
-void *sum_1_watek( void *arg){
-    Watek *watek = arg;
-    ll suma=0;
 
-    for (int i=watek->start; i<watek->end; i++) {
-        suma += watek->tab[i];
-    }
+void* sum_subarray(void *arg) {
+    ThreadData *data = arg;
+    ll sum = 0;
+    for (long i = data->start; i < data->end; ++i)
+        sum += data->arr[i];
 
-    watek->result = suma;
+    *(data->result) = sum;
     pthread_exit(NULL);
 }
 
-
-double czas() {   //funkcja do liczenia czasu wykonania operacji
+double get_time_in_microseconds() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return (double)tv.tv_sec * 1000000 + (double)tv.tv_usec; //microsekundy
+    return (double)tv.tv_sec * 1000000 + (double)tv.tv_usec;
 }
 
+double measure_time(long n, long k, long t) {
+    long total = n * k;
+    int *arr = malloc(sizeof(int) * total);
+
+    srand((unsigned)time(NULL));
+    for (long i = 0; i < total; ++i)
+        arr[i] = rand() % 10;
+
+
+    ll *k_ele = malloc(sizeof(ll) * k);
+
+    pthread_t threads[t];
+    ThreadData thread_data[t];
+
+    long subarrays_per_thread = k / t;
+    long remaining_subarrays = k % t;
+    long current_subarray = 0;
+
+    double start = get_time_in_microseconds();
+
+    for (long i = 0; i < t; ++i) {
+        long subarrays_to_sum = subarrays_per_thread + (i < remaining_subarrays ? 1 : 0);
+        thread_data[i].arr = arr;
+        thread_data[i].start = current_subarray * n;
+        thread_data[i].end = thread_data[i].start + subarrays_to_sum * n;
+        thread_data[i].result = &k_ele[current_subarray];
+        current_subarray += subarrays_to_sum;
+
+        pthread_create(&threads[i], NULL, sum_subarray, &thread_data[i]);
+    }
+
+    for (long i = 0; i < t; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    double end = get_time_in_microseconds();
+
+    free(arr);
+    free(k_ele);
+    return end - start;
+}
 
 int main(int argc, char *argv[]) {
+    long n_values[] = {1000, 5000, 10000, 20000, 50000};
+    long k_values[] = {10, 50, 100, 200, 500};
+    long t_values[] = {1, 2, 4, 8, 16};
 
-    if (argc != 4) {
-        printf("Użycie: %s <n> <t> <k>\n", argv[0]);
-        return 1;
+    FILE *fp = fopen("wyniki_nkt.csv", "w");
+
+    for (size_t i = 0; i < sizeof(n_values) / sizeof(n_values[0]); ++i) {
+        for (size_t j = 0; j < sizeof(k_values) / sizeof(k_values[0]); ++j) {
+            for (size_t l = 0; l < sizeof(t_values) / sizeof(t_values[0]); ++l) {
+                long n = n_values[i];
+                long k = k_values[j];
+                long t = t_values[l];
+
+                double time = measure_time(n, k, t);
+                if (time >= 0) {
+                    printf("n=%ld, k=%ld, t=%ld: %.2f mikrosekund\n", n, k, t, time);
+                    fprintf(fp, "%ld,%ld,%ld,%.2f\n", n, k, t, time);
+                }
+            }
+        }
     }
 
-    int n = atoi(argv[1]);
-    int k = atoi(argv[2]);
-    int t = atoi(argv[3]);
-    int total=n*k;
-    if (n <= 0 || t <= 0 || k <= 0) {
-        printf("Wartości muszą być dodatnie.\n");
-        return 1;
-    }
-    int *tab=malloc(sizeof(int)*total);  //tablica wszystkich liczb
+    fclose(fp);
 
-    srand(time(NULL));
-    for(int i=0; i<total; i++){
-        tab[i]=rand()%10;
-    }
-
-    double start1=czas();
-
-    ll suma=0;
-    for(int i=0; i<total; i++){
-        suma+=tab[i];
-    }
-
-    // for(int i=0; i<total; i++){
-    //     printf("%d ",tab[i]);
-    // }
-
-    double end1 = czas();
-    printf("\nSuma: %d ", suma);
-    printf("\nCzas sekwencyjny: %f ", end1-start1);
-
-    FILE *fp = fopen("wyniki.csv", "w");
-    if (!fp) {
-        printf("Błąd otwarcia pliku.\n");
-        return 1;
-    }
-    fprintf(fp, "t,czas_watkow\n");
-
-    /*wątki*/
-    long  il_tab_1_wat = total/t;
-    long pozostale_wat= total%t;
-    int idx=0;
-
-    pthread_t watki[t];  // tworzymy tablice wątków
-    Watek *watki_data[t]; // dane watkow
-
-    double start2=czas();
-    for (int i=0; i<t; i++) {
-        watki_data[i] =malloc(sizeof(Watek));
-        long new_il_tab=il_tab_1_wat+(pozostale_wat>0) ? 1 : 0;
-        watki_data[i]->tab=tab;
-        watki_data[i]->start=idx*n;
-        watki_data[i]->end=watki_data[i]->start+new_il_tab *n;
-        idx+=new_il_tab;
-
-        pthread_create(&watki[i], NULL, sum_1_watek, &watki_data[i]);
-        pozostale_wat--;
-        // free(watek);
-    }
-
-
-    for (int i=0; i<t; i++) {
-        pthread_join(watki[i], NULL);     //wątek glowny czeka na zakonczenie wszystkich watkow
-    }
-    double end2=czas();
-
-
-    for (int i = 0; i < t; i++) {
-        suma_watków += watki_data[i]->result;
-        free(watki_data[i]);
-    }
-
-    printf("\n Suma: %lld", suma_watków);
-    printf("\nCzas watkowy: %f ", end2-start2);
-
-
-
-    free(tab);
     return 0;
 }
